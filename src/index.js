59fb42ae73260e797fe6a09179a0c8ce93ca4d5e
Miki <ghermaico135@gmail.com>
2023-05-16 19:34:42 +0300

    Adding interactive clear and checklist

--- src/index.js
+++ src/index.js
@@ -1,41 +1,82 @@
 /** @format */
 
-import './style.css';
-import store from './modules/storage.js';
-import {
-  addTask, displayTask, clearField, input,
-} from './modules/utility.js';
+import "./style.css";
+import store from "./modules/storage.js";
+import { addTask, displayTask, clearField, input } from "./modules/utility.js";
 
-const addBtn = document.querySelector('#addBtn');
+const addBtn = document.querySelector("#addBtn");
+const clearBtn = document.querySelector("#clearBtn");
 
-window.addEventListener('load', () => {
-  displayTask(store.tasks);
+window.addEventListener("load", () => {
+	displayTask(store.tasks);
 });
 
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+addBtn.addEventListener("click", (e) => {
+	e.preventDefault();
 
-  if (input.value === '') {
-    // console.log("All Fields are required");
-  } else {
-    addTask(store.tasks);
-    displayTask(store.tasks);
-    clearField();
-  }
+	if (input.value === "") {
+		// console.log("All Fields are required");
+	} else {
+		addTask(store.tasks);
+		displayTask(store.tasks);
+		clearField();
+	}
 });
 
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('trashBtn')) {
-    const removeTask = (index) => {
-      store.tasks = store.tasks.filter(
-        (task) => Number(task.index) !== Number(index),
-      );
-      localStorage.setItem('tasks', JSON.stringify(store.tasks));
-    };
+document.addEventListener("click", (e) => {
+	if (e.target.classList.contains("trashBtn")) {
+		const removeTask = (index) => {
+			store.tasks = store.tasks.filter(
+				(task) => Number(task.index) !== Number(index)
+			);
+			localStorage.setItem("tasks", JSON.stringify(store.tasks));
+		};
 
-    removeTask(e.target.id);
-    displayTask(store.tasks);
-  }
+		removeTask(e.target.id);
+		displayTask(store.tasks);
+	}
+});
+
+// Edit
+document.addEventListener("change", () => {
+	const paragraph = document.querySelectorAll(".description");
+	paragraph.forEach((p) => {
+		p.addEventListener("keyup", (e) => {
+			store.tasks.forEach(() => {
+				const newValue = e.target.input.value;
+				console.log(input.value);
+				store.tasks.description = newValue;
+				localStorage.setItem("tasks", JSON.stringify(store.tasks));
+				// localStorage.setItem("todoTask", JSON.stringify(todoTask));
+			});
+		});
+	});
+});
+
+//checklist
+document.addEventListener("click", (e) => {
+	if (e.target.classList.contains("checkBtn")) {
+		store.tasks.forEach((task) => {
+			if (task.index === +e.target.id) {
+				if (e.target.checked) {
+					task.completed = true;
+				} else {
+					task.completed = false;
+				}
+			}
+		});
+
+		localStorage.setItem("tasks", JSON.stringify(store.tasks));
+
+		displayTask(store.tasks);
+	}
+});
+
+//clearALL
+clearBtn.addEventListener("click", () => {
+	store.tasks = store.tasks.filter((task) => task.completed !== true);
+	localStorage.setItem("tasks", JSON.stringify(store.tasks));
+	displayTask(store.tasks);
 });
 
 export default store;
--- src/modules/utility.js
+++ src/modules/utility.js
@@ -1,50 +1,45 @@
 /** @format */
 
-// import Storage from './storage.js';
-// import { store } from '../index.js';
-
-const todoListContainer = document.querySelector('#todo-list-container');
-const input = document.querySelector('#textInput');
+const todoListContainer = document.querySelector("#todo-list-container");
+const input = document.querySelector("#textInput");
 
 const addTask = (tasks) => {
-  tasks.push({
-    index: tasks.length + 1,
-    description: input.value,
-    completed: false,
-  });
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+	tasks.push({
+		index: tasks.length + 1,
+		description: input.value,
+		completed: false,
+	});
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 const displayTask = (tasks) => {
-  let content = '';
-  tasks.forEach((task, index) => {
-    task.index = index + 1;
-    content += `
+	let content = "";
+	tasks.forEach((task, index) => {
+		task.index = index + 1;
+		content += `
 <div class="todo-list"  id="${task.index}">
 <div class="task-list">
 <input type="checkbox"  class="checkBtn" ${
-  task.completed ? 'checked' : null
-} id="${task.index}"/>
+			task.completed ? "checked" : null
+		} id="${task.index}"/>
 <input type="text" id="${task.index}" class="description text-value"  value="${
-  task.description
-}" />
+			task.description
+		}" />
 </div>
 <div>
 <img class="trashBtn" src="https://img.icons8.com/plasticine/100/trash--v1.png" alt="trash--v1" id="${
-  task.index
-}"/>
+			task.index
+		}"/>
 </div>
 </div>
 `;
-  });
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  todoListContainer.innerHTML = content;
+	});
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	todoListContainer.innerHTML = content;
 };
 
 const clearField = () => {
-  input.value = '';
+	input.value = "";
 };
 
-export {
-  addTask, displayTask, clearField, input,
-};
+export { addTask, displayTask, clearField, input };
