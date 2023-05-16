3f571649cd86e3c221d9ddf7e21b84f726e2340f
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-05-16 17:56:23 +0330

    Fix remove button error

--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
 /** @format */
 
-import "./style.css";
-import Storage from "./modules/storage.js";
+import './style.css';
+import Storage from './modules/storage.js';
 import {
-	addTask,
-	removeTask,
-	displayTask,
-	clearField,
-	input,
-} from "./modules/utility.js";
+  addTask,
+  removeTask,
+  displayTask,
+  clearField,
+  input,
+} from './modules/utility.js';
 
-const addBtn = document.querySelector("#addBtn");
+const addBtn = document.querySelector('#addBtn');
 
 const store = new Storage();
-const tasks = store.getItem();
+const tasks = store.tasks;
 
-window.addEventListener("load", () => {
-	displayTask(tasks);
+window.addEventListener('load', () => {
+  displayTask(tasks);
 });
 
-addBtn.addEventListener("click", (e) => {
-	e.preventDefault();
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 
-	if (input.value == "") {
-		console.log("All Fields are required");
-	} else {
-		addTask(tasks);
-		displayTask(tasks);
-		clearField();
-	}
+  if (input.value == '') {
+    console.log('All Fields are required');
+  } else {
+    addTask(tasks);
+    displayTask(tasks);
+    clearField();
+  }
 });
 
-document.addEventListener("click", (e) => {
-	if (e.target.classList.contains("trashBtn")) {
-		removeTask(e.target.id);
-		displayTask(tasks);
-	}
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('trashBtn')) {
+    const removeTask = (index) => {
+      // let tasks = /;
+      // console.log(tasks);
+      store.tasks = store.tasks.filter(
+        (task) => Number(task.index) !== Number(index)
+      );
+
+      console.log(store.tasks);
+      store.setItem(store.tasks);
+    };
+    removeTask(e.target.id);
+    displayTask(store.tasks);
+  }
 });
+
+export { store };
--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,14 +1,13 @@
 /** @format */
 
 class Storage {
-	getItem() {
-		const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-		return tasks;
-	}
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  }
 
-	setItem(tasks) {
-		localStorage.setItem("tasks", JSON.stringify(tasks));
-	}
+  setItem(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 }
 
 export default Storage;
