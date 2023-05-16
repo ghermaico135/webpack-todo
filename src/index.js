8bbc1f7738690484215d0df1e15eb6dd589d78f6
Miki <ghermaico135@gmail.com>
2023-05-16 18:22:44 +0300

    Fixing linter

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 /** @format */
 
-import "./style.css";
-import store from "./modules/storage.js";
-import { addTask, displayTask, clearField, input } from "./modules/utility.js";
+import './style.css';
+import store from './modules/storage.js';
+import {
+  addTask, displayTask, clearField, input,
+} from './modules/utility.js';
 
-const addBtn = document.querySelector("#addBtn");
+const addBtn = document.querySelector('#addBtn');
 
-window.addEventListener("load", () => {
-	displayTask(store.tasks);
+window.addEventListener('load', () => {
+  displayTask(store.tasks);
 });
 
-addBtn.addEventListener("click", (e) => {
-	e.preventDefault();
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
 
-	if (input.value === "") {
-		// console.log("All Fields are required");
-	} else {
-		addTask(store.tasks);
-		displayTask(store.tasks);
-		clearField();
-	}
+  if (input.value === '') {
+    // console.log("All Fields are required");
+  } else {
+    addTask(store.tasks);
+    displayTask(store.tasks);
+    clearField();
+  }
 });
 
-document.addEventListener("click", (e) => {
-	if (e.target.classList.contains("trashBtn")) {
-		const removeTask = (index) => {
-			store.tasks = store.tasks.filter(
-				(task) => Number(task.index) !== Number(index)
-			);
-			localStorage.setItem("tasks", JSON.stringify(store.tasks));
-		};
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('trashBtn')) {
+    const removeTask = (index) => {
+      store.tasks = store.tasks.filter(
+        (task) => Number(task.index) !== Number(index),
+      );
+      localStorage.setItem('tasks', JSON.stringify(store.tasks));
+    };
 
-		removeTask(e.target.id);
-		displayTask(store.tasks);
-	}
+    removeTask(e.target.id);
+    displayTask(store.tasks);
+  }
 });
 
 export default store;
